2779a9272c03550b063873983253fd07a528c611
Maxime Gervilla <138915194+ItsMaxou1@users.noreply.github.com>
2026-03-12 00:00:34 +0100

    Create about.jsx

---
+++ frontend/src/pages/about.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Navbar from "../components/Home/Navbar/Navbar";
+import Footer from "../components/Home/Footer/Footer";
+import About from "../components/Home/About/About";
+
+const AboutPage = () => {
+    return (
+        <div>
+            <Navbar />
+            <About />
+            <Footer />
+        </div>
+    );
+};
+
+export default AboutPage;
