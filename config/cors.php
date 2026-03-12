7c00629f3c9e30870cb97991e77a005910982af8
GervillaMaxime <jeuxmaxime1@gmail.com>
2026-03-12 10:35:33 +0100

    fix cors.php

--- config/cors.php
+++ config/cors.php
@@ -8,6 +8,7 @@ return [
     'allowed_origins' => [
         'http://localhost:5173',
         'http://localhost:3000',
+        'https://bloc-4-frontend-production.up.railway.app',
     ],
 
     'allowed_origins_patterns' => [],
